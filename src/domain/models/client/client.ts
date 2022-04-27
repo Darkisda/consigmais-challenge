88fdf669e585de3d13da43ad311dcce759471d12
Darkisda <luandesouzanascimento2011@gmail.com>
2022-04-27 17:22:12 -0300

    refact: new attributes to client model

--- src/domain/models/client/client.ts
+++ src/domain/models/client/client.ts
@@ -1,7 +1,19 @@
+import { AccountBank } from '../accountBank';
+
 export type Client = {
   fullName: string;
   birthDate: string;
   cpf: string;
   telNumber: string;
-  cpe: string;
+  cep: string;
+  accountBank: AccountBank;
+  rg: string;
+  issuingAgency: string;
+  ufDispatch: string;
+  streetName: string;
+  houseNumber: number;
+  state: string;
+  neighborhood: string;
+  city: string;
+  motherName: string;
 };
